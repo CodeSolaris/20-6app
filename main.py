7074ebcc49f2b99753dfb946126beea15c2185be
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-18 13:39:49 +0200

    initial commit Add new API endpoint for retrieving temperature data by station and date

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
-def api_v1(station, date):
-    pd.read_csv("data.csv")
-    return render_template("api_v1.html", station=station, date=date)
+def about(station, date):
+    filename = 'data-small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    print(station)
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 if __name__ == "__main__":
