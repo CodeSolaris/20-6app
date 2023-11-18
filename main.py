7ef97aa633bb9f990d42e9a5eede115fc2bb90a6
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-18 22:29:14 +0200

    add url endpoint for yearly and station data

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-stations  = pd.read_csv('data-small/stations.txt', skiprows=17)
-stations = stations[['STAID', 'STANAME                                 ']]
+stations = pd.read_csv("data-small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -12,13 +14,27 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    filename = 'data-small/TG_STAID' + str(station).zfill(6) + '.txt'
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
-    print(station)
-    return {"station": station,
-            "date": date,
-            "temperature": temperature}
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    return {"station": station, "date": date, "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def station_data(station, date):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def station_data_per_year(station, year):
+    filename = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df.loc[df["    DATE"].str.startswith(year)].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
